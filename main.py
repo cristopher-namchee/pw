c3e6d4d73be6d96e4a9f284b0f2f34b68e3fe169
Cristopher Namchee <cristophernamchee12@gmail.com>
2024-08-22 14:10:18 +0700

    feat: integrate with VPN

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import re
-import subprocess
 import sys
+import time
 
 import pexpect
 import paramiko
@@ -9,6 +9,33 @@ import paramiko
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
+def connect_forticlient_vpn(vpn_host, vpn_port, vpn_username, vpn_password):
+    command = f"./client/forticlientsslvpn_cli --server {vpn_host}:{vpn_port} --vpnuser {vpn_username}"
+    print(command)
+
+    vpn = pexpect.spawn(command)
+    vpn.logfile = sys.stdout.buffer
+
+    vpn.expect("Password for VPN:")
+    vpn.sendline(vpn_password)
+
+    vpn.expect(
+        re.compile(
+            r"Would you like to connect to this server\? \(Y/N\)", re.IGNORECASE
+        ),
+    )
+    vpn.sendline("Y")
+
+    vpn.expect("Press Ctrl-C to quit")
+
+    if vpn.isalive():
+        print("VPN connected successfully.")
+    else:
+        print("Failed to establish the VPN connection.")
+
+    return vpn
 
 
 def format_sql_table(data: str) -> str:
@@ -35,71 +62,47 @@ def format_sql_table(data: str) -> str:
 
     table.append(f"+{'-' * (max_status_length + 2)}+{'-' * (max_count_length + 2)}+")
 
-    # Join the table rows with newline characters
     return "\n".join(table)
 
 
-# vpn_host = os.getenv("VPN_HOST")
-# vpn_port = os.getenv("VPN_PORT")
-# vpn_user = os.getenv("VPN_USER")
-# vpn_password = os.getenv("VPN_PASSWORD")
-
-# child = pexpect.spawn(
-#     f"./client/forticlientsslvpn_cli --server {vpn_host}:{vpn_port} --vpnuser {vpn_user}"
-# )
-
-# child.expect("Password for VPN:")
-# child.sendline(vpn_password)
-
-# child.expect(
-#     re.compile(r"Would you like to connect to this server\? \(Y/N\)", re.IGNORECASE)
-# )
-# child.sendline("Y")
-
-# child.expect("Press Ctrl-C to quit")
-
-# child = pexpect.spawn(f"ssh {os.getenv('SSH_USER')}@{os.getenv('SSH_HOST')}")
-# child.logfile = sys.stdout.buffer
-
-# child.expect(f"{os.getenv('SSH_USER')}@{os.getenv('SSH_HOST')}'s password:")
-# child.sendline(os.getenv("SSH_PASSWORD"))
-
-# child.interact()
-
 ssh = paramiko.SSHClient()
-ssh.load_system_host_keys()
-paramiko.util.log_to_file("ssh.log")
-
-print("paramiko created")
-
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(
-    hostname=os.getenv("SSH_HOST"),
-    username=os.getenv("SSH_USER"),
-    password=os.getenv("SSH_PASSWORD"),
+
+process = connect_forticlient_vpn(
+    os.getenv("VPN_HOST"),
+    os.getenv("VPN_PORT"),
+    os.getenv("VPN_USER"),
+    os.getenv("VPN_PASSWORD"),
 )
 
-print("SSH connected!")
+time.sleep(30)
 
-stdin, stdout, stderr = ssh.exec_command("~/dpo_logs/count_queue.sh")
-stdout.channel.set_combine_stderr(True)
-queue = stdout.read().decode()
+if process and process.isalive():
+    ssh.connect(
+        hostname=os.getenv("SSH_HOST"),
+        username=os.getenv("SSH_USER"),
+        password=os.getenv("SSH_PASSWORD"),
+    )
 
-stdin, stdout, stderr = ssh.exec_command("df -h")
-stdout.channel.set_combine_stderr(True)
-disk = stdout.read().decode()
+    stdin, stdout, stderr = ssh.exec_command("~/dpo_logs/count_queue.sh")
+    stdout.channel.set_combine_stderr(True)
+    queue = stdout.read().decode()
 
-stdin, stdout, stderr = ssh.exec_command(
-    "docker exec datasaur-mariadb bash -c 'mysql -h$DATABASE_HOST -u$DATABASE_USERNAME -p$DATABASE_PASSWORD -e \"use datasaur; select status, count(*) from llm_vector_store_document WHERE llmVectorStoreId=9 GROUP BY status;\"'",
-    get_pty=True,
-)
-stdout.channel.set_combine_stderr(True)
-document_count = stdout.read().decode()
+    stdin, stdout, stderr = ssh.exec_command("df -h")
+    stdout.channel.set_combine_stderr(True)
+    disk = stdout.read().decode()
 
-print(queue)
-print(disk)
-print(format_sql_table(document_count))
+    stdin, stdout, stderr = ssh.exec_command(
+        "docker exec datasaur-mariadb bash -c 'mysql -h$DATABASE_HOST -u$DATABASE_USERNAME -p$DATABASE_PASSWORD -e \"use datasaur; select status, count(*) from llm_vector_store_document WHERE llmVectorStoreId=9 GROUP BY status;\"'",
+        get_pty=True,
+    )
+    stdout.channel.set_combine_stderr(True)
+    document_count = stdout.read().decode()
 
-if ssh is not None:
-    ssh.close()
-    del ssh, stdin, stdout, stderr
+    print(queue)
+    print(disk)
+    print(format_sql_table(document_count))
+
+    if ssh is not None:
+        ssh.close()
+        del ssh, stdin, stdout, stderr
